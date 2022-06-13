04168dfdb5c5cee5c70e55d24e3d232976dd38da
Steve Bartholomew <steveb@motabilityoperations.co.uk>
2022-06-13 09:56:00 +0100

    first commit

--- e2e/src/step-definitions/hooks/hooks.ts
+++ e2e/src/step-definitions/hooks/hooks.ts
@@ -11,11 +11,23 @@ AfterAll(async ()=>{
     await global.browser.close();
 });
 
-Before(async()=>{
+Before(async(scenario)=>{
     global.context = await global.browser.newContext(); //an isolated incognito type session within a browser instance - run each test scenario in its own browser context
+    global.context = await global.browser.newContext({
+        recordVideo: {
+            dir: './reports/videos/' + scenario.pickle.name,
+
+        }
+    })
     global.page = await global.context.newPage(); //a page refers to a single tab or pop up window within a browser context
 })
 
-After(async()=>{
+After(async(scenario)=>{
+    const scenarioStatus = scenario.result?.status;
+    if(scenarioStatus === 'FAILED'){
+        await global.page.screenshot({
+            path: `./reports/screenshots/${scenario.pickle.name}.png`
+        })
+    }
     await global.page.close();
 })
